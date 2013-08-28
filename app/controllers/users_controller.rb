4df79b33ad2ec36a6f5c476b6161361db36edd7b
jdeslangles <julien.deslangles@gmail.com>
2013-08-28 16:00:43 +0100

    Working devise model,views and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @trips = Trip.where(user_id: @user.id)
+    @trips = @user.trips
 
     respond_to do |format|
       format.html # show.html.erb
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ TravelBlog::Application.routes.draw do
 
   get '/my_profile', to: 'users#my_profile', as: :my_profile
 
+  root :to => 'users#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -60,7 +62,7 @@ TravelBlog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   # root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
 
