ce0fe7db9847d86389e1c5536b0ac169fea9122c
jdeslangles <julien.deslangles@gmail.com>
2013-08-29 08:59:03 +0100

    fixed uploader bug, cleaned file check methods

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,8 +2,17 @@ class Photo < ActiveRecord::Base
   belongs_to :marker
   attr_accessible :description, :name, :photo_picture, :marker_id
 
+  mount_uploader :photo_picture, PhotoPictureUploader
+
+  validates :photo_picture, presence: true
+
+  validate :photo_picture_size_validation
 
 
+  private
+  def photo_picture_size_validation
+    errors[:photo_picture, "Photo"] << "should be less than 1MB" if photo_picture.size > (1.2).megabyte
+  end
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,43 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   has_many :trips
-  attr_accessible :bio, :birthdate, :email, :firstname, :lastname, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :bio, :birthdate, :email, :firstname, :lastname, :password, :password_confirmation, :remember_me, :username, :avatar, :location
+
+
+  mount_uploader :avatar, UserAvatarUploader
+
+  validate :avatar_size_validation
+
+  private
+  def downcase_username
+    self.username.downcase! if self.username
+  end
+
+  private
+  def set_default_role
+    self.role ||= 'registered'
+  end
+
+
+  private
+  def avatar_size_validation
+    errors[:avatar] << "should be less than 1MB" if avatar.size > (1.2).megabyte
+
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
