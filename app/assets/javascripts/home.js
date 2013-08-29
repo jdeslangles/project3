4483f5f91cb6f388dffdb46ac4c6616897a79f89
bj <britneyjo.ludkowski@gmail.com>
2013-08-29 23:32:18 +0100

    trying to get ajax search to work

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,19 @@
-function showHide(search_results_all) {
-  var element = document.getElementById("search_results_all");
-  if (element.style.visibility == 'hidden') {
-      element.style.visibility = 'visible';
-  } else if (element.style.visibility == 'visible') {
-      element.style.visibility = 'hidden';
+// function showHide(search_results_all) {
+//   var element = document.getElementById("search_results_all");
+//     // e.preventDefault();
+//     element.style.display = "block";
+//     return false;
+// };
+
+
+  // $('#submit_form_button').on("submit", function (e) {
+  //   e.preventDefault();
+  //   $('#search_results_all').show();
+  // });
+
+  $('#submit_form_button').on("submit", function showHide(event) {
+    event.preventDefault();
+    $('#search_results_all').show();
   };
-};
+
+
