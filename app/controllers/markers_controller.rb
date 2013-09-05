5f1f126e93f58c61702be223bdbbd860bccb3abf
bj <britneyjo.ludkowski@gmail.com>
2013-09-05 08:20:51 +0100

    redirect to root after delete of anything

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -83,7 +83,7 @@ class MarkersController < ApplicationController
     @marker.destroy
 
     respond_to do |format|
-      format.html { redirect_to markers_url }
+      format.html { redirect_to root_url }
       format.json { head :no_content }
     end
   end
