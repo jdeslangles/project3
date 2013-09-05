03fa9bdb65e8b32258c6768265815196a3f3c139
jdeslangles <julien.deslangles@gmail.com>
2013-09-05 11:15:29 +0100

    some progress on pdf

--- app/pdfs/trip_pdf.rb
+++ app/pdfs/trip_pdf.rb
@@ -1,10 +1,16 @@
 class TripPdf
   def initialize(trip)
     @trip = trip
-    @trip = Prawn::Document.new
+    @pdf = Prawn::Document.new
   end
   def render
-    @trip.text "Hello!"
-    @trip.render
+    @pdf.text @trip.name
+
+    @trip.markers.each do |marker|
+
+    end
+
+    @pdf.image open("https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&sensor=false")
+    @pdf.render
   end
 end
--- config/initializers/prawn.rb
+++ config/initializers/prawn.rb
@@ -1,1 +1,2 @@
 require 'prawn'
+require 'open-uri'
